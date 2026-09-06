156ce3fa211167a5ce20471645f6fb886948310e
murcafes94 <esaumuri94@gmail.com>
2026-09-06 11:59:56 -0500

    Crear ejecutable inicial MurSchol Files

---
+++ desktop/os/shell/src/files_main.cpp
@@ -0,0 +1,24 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+
+#include "FileListModel.h"
+#include "ThemeIconProvider.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+    app.setApplicationName("MurSchol Files");
+    app.setOrganizationName("MurSchol");
+    QQuickStyle::setStyle("Basic");
+
+    qmlRegisterType<FileListModel>("MurScholFiles", 1, 0, "FileListModel");
+
+    QQmlApplicationEngine engine;
+    engine.addImageProvider(QStringLiteral("theme"), new ThemeIconProvider);
+    engine.load(QUrl(QStringLiteral("qrc:/qml/Files.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
