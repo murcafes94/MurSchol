f0fa7165212a6954a3f5fba4e2cc248044f3bf29
murcafes94 <esaumuri94@gmail.com>
2026-09-06 10:06:01 -0500

    Resolver también rutas absolutas de iconos

--- desktop/os/shell/src/ThemeIconProvider.cpp
+++ desktop/os/shell/src/ThemeIconProvider.cpp
@@ -1,7 +1,9 @@
 #include "ThemeIconProvider.h"
 
+#include <QFileInfo>
 #include <QIcon>
 #include <QPixmap>
+#include <QUrl>
 
 ThemeIconProvider::ThemeIconProvider()
     : QQuickImageProvider(QQuickImageProvider::Pixmap)
@@ -14,7 +16,17 @@ QPixmap ThemeIconProvider::requestPixmap(const QString &id, QSize *size, const Q
     if (target.width() <= 0 || target.height() <= 0)
         target = QSize(48, 48);
 
-    QIcon icon = QIcon::fromTheme(id);
+    QString iconId = QUrl::fromPercentEncoding(id.toUtf8());
+    QIcon icon;
+
+    // Algunos archivos .desktop entregan un nombre de tema y otros una ruta
+    // absoluta a PNG/SVG. MurSchol acepta ambos para no degradar aplicaciones
+    // instaladas fuera de los repositorios Debian.
+    if (QFileInfo::exists(iconId))
+        icon = QIcon(iconId);
+    else
+        icon = QIcon::fromTheme(iconId);
+
     if (icon.isNull())
         icon = QIcon::fromTheme(QStringLiteral("application-x-executable"));
 
