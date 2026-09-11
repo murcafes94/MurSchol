95a37c1a38bed3776dd2e9ff6bc6da5c5b6de7c3
murcafes94 <esaumuri94@gmail.com>
2026-09-11 18:16:24 -0500

    reader: open files passed from desktop

--- desktop/apps/reader/src/main.cpp
+++ desktop/apps/reader/src/main.cpp
@@ -1,5 +1,7 @@
+#include <QFileInfo>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include <QQuickStyle>
 #include <QUrl>
 
@@ -10,7 +12,16 @@ int main(int argc, char *argv[])
     QGuiApplication::setOrganizationName(QStringLiteral("MurSchol"));
     QQuickStyle::setStyle(QStringLiteral("Basic"));
 
+    QUrl startupDocument;
+    const QStringList arguments = QCoreApplication::arguments();
+    if (arguments.size() > 1) {
+        const QFileInfo candidate(arguments.at(1));
+        if (candidate.exists() && candidate.isFile())
+            startupDocument = QUrl::fromLocalFile(candidate.absoluteFilePath());
+    }
+
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty(QStringLiteral("murscholStartupDocument"), startupDocument);
     engine.load(QUrl(QStringLiteral("qrc:/MurScholReader/qml/Main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
