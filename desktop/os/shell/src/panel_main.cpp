d926850806bbae803a20368301b2664139642af7
murcafes94 <esaumuri94@gmail.com>
2026-09-14 11:50:48 -0500

    shell: register quick controls in panel

--- desktop/os/shell/src/panel_main.cpp
+++ desktop/os/shell/src/panel_main.cpp
@@ -8,6 +8,7 @@
 #include <LayerShellQt/Window>
 
 #include "SystemBackend.h"
+#include "QuickControlsBackend.h"
 #include "AppIndexModel.h"
 #include "UniversalSearchModel.h"
 #include "AppManagerBackend.h"
@@ -27,6 +28,7 @@ int main(int argc, char *argv[])
     QQuickWindow::setDefaultAlphaBuffer(true);
 
     qmlRegisterType<SystemBackend>("MurScholShell", 1, 0, "SystemBackend");
+    qmlRegisterType<QuickControlsBackend>("MurScholShell", 1, 0, "QuickControlsBackend");
     qmlRegisterType<AppIndexModel>("MurScholShell", 1, 0, "AppIndexModel");
     qmlRegisterType<UniversalSearchModel>("MurScholShell", 1, 0, "UniversalSearchModel");
     qmlRegisterType<AppManagerBackend>("MurScholShell", 1, 0, "AppManagerBackend");
